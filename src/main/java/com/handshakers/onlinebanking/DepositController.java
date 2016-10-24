866f373e85d9ea1d6e95e86eb8007ea92a67b0f4
Nishant Balami <nishant.ggmu@gmail.com>
2016-10-24 01:59:26 -0500

    Deposit Controller is added.

---
+++ src/main/java/com/handshakers/onlinebanking/DepositController.java
@@ -0,0 +1,27 @@
+package com.handshakers.onlinebanking;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DepositController {
+	
+//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@RequestMapping(value = "/deposit", method = RequestMethod.GET)
+	public String getDepositAmount(Locale locale, Model model) {
+		return "depositAmount";
+	}
+	
+	@RequestMapping(value = "/deposit", method = RequestMethod.POST)
+	public String saveDepositAmount(Locale locale, Model model) {
+		return "depositAmount";
+	}
+}
